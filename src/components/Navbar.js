373bcd6aed11b98f683f779796df8f3b56d302e9
Boomika1506 <82639613+Boomika1506@users.noreply.github.com>
2026-07-22 15:18:57 +1200

    Fix ESLint warnings and remove router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
-    <nav className={`navbar navbar-expand-lg navbar-${props.mode } bg-${props.mode}`}>
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-         {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-         <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-       {/* <li className="nav-item">
-          <Link className="nav-link" to="/about">{props.aboutText}</Link>
-        </li> */}
-      </ul>
-      <div className={`form-check form-switch text-${props.mode==='light'? 'dark' : 'light'}`}>
-      <input className="form-check-input"  onClick={props.toggleMode} type="checkbox" role="switch" id="switchCheckDefault"/>
-      <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-    </div>
-    </div>
-  </div>
-</nav>
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <div className="container-fluid">
+        {/* Changed to a span so it behaves like text without needing a broken href link */}
+        <span className="navbar-brand mb-0 h1">{props.title}</span>
+        
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              {/* Changed Home to a simple text item for now since we aren't routing */}
+              <span className="nav-link active" style={{ cursor: 'pointer' }}>Home</span>
+            </li>
+          </ul>
+          
+          <div className={`form-check form-switch text-${props.mode==='light'? 'dark' : 'light'}`}>
+            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="switchCheckDefault"/>
+            <label className="form-check-label" htmlFor="switchCheckDefault">Enable Dark Mode</label>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
